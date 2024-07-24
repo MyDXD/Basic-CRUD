16a80dfddc4925ce01152dc424def0a18d8d7d47
Hassadin Sasoh <dislove107@gmail.com>
2024-07-24 15:42:29 +0700

    start server && set gitignore

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const books = [
+  { id: 1, title: "Book 1", author: "Author 1" },
+  { id: 2, title: "Book 2", author: "Author 2" },
+  { id: 3, title: "Book 3", author: "Author 3" },
+];
+
+// Endpoint to get all books
+app.get("/api/books", (req, res) => {
+  res.json(books);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
